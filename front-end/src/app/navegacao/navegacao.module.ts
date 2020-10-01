b01ef673f0fe3b965242fe90d6b32619b3df6558
giorgiocost <gegiorgio1991@gmail.com>
2020-09-30 23:22:20 -0300

    components

--- front-end/src/app/navegacao/navegacao.module.ts
+++ front-end/src/app/navegacao/navegacao.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
-    declarations: [],
+    declarations: [FooterComponent, HomeComponent, MenuComponent, NotFoundComponent],
     imports: [ CommonModule, RouterModule ],
     exports: [],
 })
