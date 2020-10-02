54a2edc02efe4ce03b6decb02d69ecaa11d83cc3
giorgiocost <gegiorgio1991@gmail.com>
2020-10-01 23:10:48 -0300

    ajuste de import footer

--- front-end/src/app/navegacao/navegacao.module.ts
+++ front-end/src/app/navegacao/navegacao.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from './footer/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -10,6 +10,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
     declarations: [FooterComponent, HomeComponent, MenuComponent, NotFoundComponent],
     imports: [ CommonModule, RouterModule ],
-    exports: [],
+    exports: [FooterComponent, HomeComponent, MenuComponent, NotFoundComponent],
 })
 export class NavegacaoModule {}
